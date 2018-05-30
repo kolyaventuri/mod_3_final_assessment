af6e4053fc62561974d0729b81bcb2f9b29481b2
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-30 10:03:52 -0600

    Serializer for game model

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::GamesController < ApplicationController
   def show
     game = Game.find(params[:id])
 
-    render json: game.to_json
+    render json: GameSerializer.new(game).to_json
   end
 end
