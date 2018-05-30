f88133952cadaf5546fdd0f1a0c678ccc3de060d
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-30 09:38:30 -0600

    Word validation controller

---
+++ app/controllers/word/validate_controller.rb
@@ -0,0 +1,4 @@
+class Word::ValidateController < ApplicationController
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "welcome#index"
+
+  post '/word/validate', to: 'word/validate#create', as: 'word_lookup'
 end
