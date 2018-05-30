a351a5c47131cc71fb76c6d68e687b047df447bc
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-30 09:34:09 -0600

    User lookup words spec

---
+++ spec/features/user/user_can_lookup_word_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature 'Any user visiting the root' do
+  context 'can enter a valid word' do
+    scenario 'and be shown its root form' do
+      visit '/'
+
+      fill_in 'word', with: 'foxes'
+      click_on 'Lookup Word'
+
+      expect(current_path).to be('/')
+      expect(page).to have_content("'foxes' is a valid word and its root form is 'fox'.")
+    end
+  end
+
+  context 'can enter an invalid word' do
+    scenario 'and be told that it is invalid' do
+      visit '/'
+
+      fill_in 'word', with: 'foxez'
+      click_on 'Lookup Word'
+
+      expect(current_path).to be('/')
+      expect(page).to have_content("'foxez' is not a valid word.")
+    end
+  end
+end
