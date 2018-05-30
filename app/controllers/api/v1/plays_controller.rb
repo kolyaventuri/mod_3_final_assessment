e6ddb7e32ea12e335c10407125f2442a9cd744b2
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-30 11:22:30 -0600

    Null session forgery protection to prevent against an invalid authenticity error

--- app/controllers/api/v1/plays_controller.rb
+++ app/controllers/api/v1/plays_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::PlaysController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def create
     game = Game.find(params[:game_id])
     user = User.find(play_params[:user_id])
