75e027bed529a33810f38cc40f0a8e1e5a4d8669
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-30 10:12:46 -0600

    Game scoring per player

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,20 @@ class Game < ApplicationRecord
   belongs_to :player_2, class_name: "User", foreign_key: :player_2_id
 
   has_many :plays
+
+  def scores
+    [
+      score_hash(player_1),
+      score_hash(player_2)
+    ]
+  end
+
+  private
+
+  def score_hash(player)
+    {
+      user_id: player.id,
+      score: plays.where(user: player).sum(:score)
+    }
+  end
 end
