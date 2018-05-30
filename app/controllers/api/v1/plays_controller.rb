8a75fff3bd654d712157d8db83a358834e4b25ea
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-30 10:32:28 -0600

    Fix play spec

--- app/controllers/api/v1/plays_controller.rb
+++ app/controllers/api/v1/plays_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::PlaysController < ApplicationController
   def create
+    game = Game.find(params[:game_id])
+    user = User.find(play_params[:user_id])
 
+    result = game.plays.create(user: user, word: play_params[:word])
+  end
+
+  private
+
+  def play_params
+    params.permit(:user_id, :word)
   end
 end
