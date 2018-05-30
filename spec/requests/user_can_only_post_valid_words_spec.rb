687bfce55b8157cf4879a32b8a039fdfe0be2f2b
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-30 10:44:16 -0600

    Spec to make sure that invalid plays are caught

--- spec/requests/user_can_only_post_valid_words_spec.rb
+++ spec/requests/user_can_only_post_valid_words_spec.rb
@@ -11,4 +11,21 @@ feature 'A user posting to /api/v1/games/:id/plays', type: :request do
       expect(response.status).to be(201)
     end
   end
+
+  context 'with an invalid word' do
+    scenario 'is returned an error' do
+      user = create(:user)
+      game = create(:game)
+
+      post "/api/v1/games/#{game.id}/plays", params: { user_id: user.id, word: 'notvalidword' }
+
+      expect(response).to_not be_successful
+      expect(response.status).to be(400)
+
+      get "/api/v1/games/#{game.id}"
+      game_data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(game_data[:scores].first[:score]).to be(0)
+    end
+  end
 end
