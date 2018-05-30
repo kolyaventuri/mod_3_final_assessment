18c1ec3dcf2dc8ecf50c61dcfe383687840e5510
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-30 10:45:51 -0600

    Spec expecting play to be able to be invalid

--- spec/models/play_spec.rb
+++ spec/models/play_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 describe Play do
+  context "Instantiation" do
+    it 'disallows invalid words' do
+      play = create(:play, word: 'thisisnotavalidword')
+      expect(play).to_not be_valid
+    end
+  end
+
   context "Instance methods" do
     context "#score_word" do
       it "scores the word" do
