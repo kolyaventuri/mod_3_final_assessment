0bd2f71b911a57e32a65c4bd7a2d4203160631d9
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-30 09:57:25 -0600

    API GamesController

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   post '/word/validate', to: 'word/validate#create', as: 'word_lookup'
+
+  namespace :api do
+    namespace :v1 do
+      resources :games, only: [:show]
+    end
+  end
 end
