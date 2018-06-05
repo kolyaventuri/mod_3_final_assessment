9f9b1ac178def4880aeb817048e9a4906c487df7
Kolya Venturi <kolya.venturi@enx3s.com>
2018-06-04 23:18:30 -0600

    Word model creation to do validation

--- app/controllers/word/validate_controller.rb
+++ app/controllers/word/validate_controller.rb
@@ -1,12 +1,14 @@
 class Word::ValidateController < ApplicationController
   def create
-    word_lookup_result = OxfordService.lookup_word(word_params[:word])
-    if word_lookup_result[:success]
-      flash[:result] = "'#{word_params[:word]}' is a valid word and its root form is '#{word_lookup_result[:root_form]}'."
-    else
-      flash[:result] = "'#{word_params[:word]}' is not a valid word."
-    end
-    redirect_to root_path
+    #word_lookup_result = OxfordService.lookup_word(word_params[:word])
+    #if word_lookup_result[:success]
+      #flash[:result] = "'#{word_params[:word]}' is a valid word and its root form is '#{word_lookup_result[:root_form]}'."
+    #else
+      #flash[:result] = "'#{word_params[:word]}' is not a valid word."
+    #end
+    #redirect_to root_path
+
+    @word = Word.new(word_params)
   end
 
   private
