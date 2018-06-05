8bb82b37e0a165a589b5bfc3f5df3919258b74f0
Kolya Venturi <kolya.venturi@enx3s.com>
2018-06-04 23:24:03 -0600

    Use VCR in word model test

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,9 +3,18 @@ class Word
 
   def initialize(word)
     @word = word
+
+    check_validity
   end
 
   def valid?
     @valid
+  end
+
+  private
+
+  def check_validity
+    result = OxfordService.lookup_word(@word)
+    @valid = result[:success]
   end
 end
--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -2,20 +2,26 @@ require 'rails_helper'
 
 describe Word, type: :model do
   it 'should contain a word' do
-    word = Word.new('fox')
+    VCR.use_cassette('word_model_valid') do
+      word = Word.new('fox')
 
-    expect(word.word).to eq('fox')
+      expect(word.word).to eq('fox')
+    end
   end
 
   it 'should validate that a word is valid' do
-    word = Word.new('fox')
+    VCR.use_cassette('word_model_valid') do
+      word = Word.new('fox')
 
-    expect(word).to be_valid
+      expect(word).to be_valid
+    end
   end
 
   it 'should be able to say a word is not valid' do
-    word = Word.new('foxz')
+    VCR.use_cassette('word_model_invalid') do
+      word = Word.new('foxz')
 
-    expect(word).to_not be_valid
+      expect(word).to_not be_valid
+    end
   end
 end
